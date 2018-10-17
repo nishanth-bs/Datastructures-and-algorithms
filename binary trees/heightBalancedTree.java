0ba65bfbcd9a3b3665f55c9273d073a018dec6b7
Nishanth BS <nishanth.star1@gmail.com>
2018-10-17 23:38:32 +0530

    BT height balanced in java

--- binary trees/heightBalancedTree.java
+++ binary trees/heightBalancedTree.java
@@ -1,41 +1,42 @@
 import java.util.*;
-import BinaryTreeNode;
-private static class BalanceStatusWithHeight{
-	boolean balanced;
-	int height;
-	public balancedStatusWithHeight(boolean balanced, int height){
-		this.balanced = balanced;
-		this.height = height;
-	}
-}
+import BinaryTreeNode.java;
 
-static boolean isBalanced( BinaryTreeNode<Integer> tree){
-	return checkBalanced(tree).balanced;
-}
-
-static BalanceStatusWithHeight checkBalanced( BinaryTreeNode<Integer> tree){
-	if(tree == null){ //base case
-		return new BalanceStatusWithHeight(true, -1);
-	}
-	
-	BalanceStatusWithHeight leftRes = checkBalanced(tree.left);
-	if(!leftRes){
-		return leftRes;		//left subtree is not balanced
-	}
-
-	BalanceStatusWithHeight rightRes = checkBalanced(tree.right);
-	if(!rightRes){
-		return rightRes;
-	}
-
-	boolean isBalanced = Math.abs(leftRes.height - rightRes.height) <= 1;
-	int height = Math.max(leftRes.height, rightRes.height) + 1;
-	return new BalanceStatusWithHeight( isBalanced, height);
-}
 
 class heightBalancedTree{
 	public static void main(String[] args){
 		BinaryTreeNode<Integer> a = new BinaryTreeNode<>();
+	}
+	
+	private static class BalanceStatusWithHeight{
+		boolean balanced;
+		int height;
+		public BalanceStatusWithHeight(boolean balanced, int height){
+			this.balanced = balanced;
+			this.height = height;
+		}
+	}
+	static boolean isBalanced( BinaryTreeNode<Integer> tree){
+		return checkBalanced(tree).balanced;
+	}
+
+	static BalanceStatusWithHeight checkBalanced( BinaryTreeNode<Integer> tree){
+		if(tree == null){ //base case
+			return new BalanceStatusWithHeight(true, -1);
+		}
+		
+		BalanceStatusWithHeight leftRes = checkBalanced(tree.left);
+		if(!leftRes){
+			return leftRes;		//left subtree is not balanced
+		}
+
+		BalanceStatusWithHeight rightRes = checkBalanced(tree.right);
+		if(!rightRes){
+			return rightRes;
+		}
+
+		boolean isBalanced = Math.abs(leftRes.height - rightRes.height) <= 1;
+		int height = Math.max(leftRes.height, rightRes.height) + 1;
+		return new BalanceStatusWithHeight( isBalanced, height);
 	}
 }
 
